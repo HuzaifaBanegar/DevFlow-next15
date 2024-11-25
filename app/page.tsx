acc643f1f37e3a1c600c5cd797ac261ac14cf006
BANEGAR HUZAIFA <banegarhuzzi1010@gmail.com>
2024-11-25 20:46:14 +0530

    Eslin and Prettier setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Welcome to Next Js 15
-    </div>
-  );
-}
+const Home = () => {
+  <h1 className="text-3xl text-white font-black">Welcome to next js 15</h1>;
+};
+
+export default Home;
