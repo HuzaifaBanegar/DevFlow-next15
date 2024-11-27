4ea177c81d591e96c2f5bb167ecaf64da3e5cddc
BANEGAR HUZAIFA <banegarhuzzi1010@gmail.com>
2024-11-27 18:02:37 +0530

    shadcn added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-// import Image from "next/image";
+import Image from "next/image";
+
 
 const Home = () => {
   return (
   <>
   <h1 className="text-3xl">Welcome to next js 15</h1>
   <h1 className="text-3xl font-space-grotesk">Welcome to next js 15</h1>
+  
+
   </>)
 };
 
