a211cab5e73da4a6d349644d045fc7dbe8e2c09b
BANEGAR HUZAIFA <banegarhuzzi1010@gmail.com>
2024-11-29 19:12:20 +0530

    form Ui complete handleSubmit left

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,10 +1,16 @@
+"use client";
+
+import AuthForm from '@/components/forms/AuthForm'
+import { SignInSchema } from '@/lib/validations'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      Signin
-    </div>
+    <AuthForm
+        formType="SIGN_IN" 
+        schema={SignInSchema} 
+        defaultValues={{email:'', password:''}} 
+        onSubmit={(data)=>{Promise.resolve({success: true, data})}}/>
   )
 }
 
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,10 +1,16 @@
+"use client";
+
+import AuthForm from '@/components/forms/AuthForm'
+import { SignUpSchema } from '@/lib/validations'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      Signup
-    </div>
+    <AuthForm
+    formType="SIGN_UP" 
+    schema={SignUpSchema} 
+    defaultValues={{email:'', password:'', name:'', username:''}} 
+    onSubmit={(data)=>{Promise.resolve({success: true, data})}}/>
   )
 }
 
