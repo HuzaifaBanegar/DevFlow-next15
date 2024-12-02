747f8ce6621eb590fbfd3abf1e1ad146d624a20e
BANEGAR HUZAIFA <banegarhuzzi1010@gmail.com>
2024-12-02 20:34:09 +0530

    mobile nav and Nav Links component complete

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import Theme from './Theme'
+import MobileNavigation from './MobileNavigation'
 
 const Navbar = () => {
   return (
@@ -11,7 +12,10 @@ const Navbar = () => {
         <p className='h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden'>Dev<span className='text-primary-500'>Flow</span></p>
         </Link>
         <p>Global search</p>
-        <div className='flex-between gap-5'><Theme/></div>
+        <div className='flex-between gap-5'>
+          <Theme/>
+          <MobileNavigation/>
+        </div>
     </nav>
   )
 }
